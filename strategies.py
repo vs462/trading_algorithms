1a339bd99c2be813a8fcf799a2cf62aa1ca1d121
vs462 <60940617+vs462@users.noreply.github.com>
2021-03-29 13:12:23 +0100

    add more strategies

--- strategies.py
+++ strategies.py
@@ -5,23 +5,29 @@ import numpy as np
 from itertools import product 
 
 
-
 ticker = ["SNOW", "AMZN", "MSFT"]#, "AMZN", "SNOW", "BA", "KO", "IBM", "DIS", "MSFT" ]
 
 stocks = yf.download(ticker, start = "2008-01-01", end = "2021-03-26")
 
 df = stocks.loc[:, "Close"].copy()
 
-data = df['SNOW'].to_frame()
+data = df['AMZN'].to_frame()
 data.columns=['price']
 
 
 def buy_and_hold(df):
+    data = df.copy()
+    data=data.to_frame()
+    data.columns=['price']
     data["returns"] = np.log(data.price.div(data.price.shift(1)))
+    data.dropna(inplace = True)
     return np.exp(data["returns"].sum())
     
+
+
 def SMA_test(df, SMA_range, min_periods=10):
     data = df.copy()
+    data.columns=['price']
     sma_s, sma_l = SMA_range[0], SMA_range[1]
     data["returns"] = np.log(data.price.div(data.price.shift(1)))
     data["SMA_S"] = data.price.rolling(int(sma_s), min_periods=min_periods).mean()
@@ -63,22 +69,49 @@ def find_best_comb(df):
     
     return best_comb, np.max(results)
 
+def cantrarian(df, window = 3):
+    data = df.copy()
+    # data=data.to_frame()
+    data.columns=['price']
+    data["returns"] = np.log(data.price.div(data.price.shift(1)))
+    data.dropna(inplace = True)    
+    data["position"] =  -np.sign(data["returns"].rolling(window).mean()) # contrarian (minus sign)
+    data.dropna(inplace = True)
+    data["strategy"] = data.position.shift(1) * data["returns"]
+    return data[["returns", "strategy"]].sum().apply(np.exp)[-1]
+
+def momentum(df, window = 3):
+    data = df.copy()
+    # data=data.to_frame()
+    data.columns=['price']
+    data["returns"] = np.log(data.price.div(data.price.shift(1)))
+    data.dropna(inplace = True)
+    data["position"] =  np.sign(data["returns"].rolling(window).mean()) # contrarian (minus sign)
+    data.dropna(inplace = True)
+    data["strategy"] = data.position.shift(1) * data["returns"]
+    return data[["returns", "strategy"]].sum().apply(np.exp)[-1]
+
 #data["position"] = np.where(data["SMA_S"] > data["SMA_L"], 1, -1 )
 
-result = SMA_test(data, (41, 100))
-result2 = buy_and_hold(data)
+# result = SMA_test(data, (41, 100))
+# result2 = buy_and_hold(data)
 
 
-print(result)
-plot_SMA(data, (50, 200), '2020-09')
+# print(result)
+# plot_SMA(data, (50, 200), '2020-09')
 
 
-best = find_best_comb(data)
+# best = find_best_comb(data)
 
-SMA_S_range = range(10,50,1)
-SMA_L_range = range(100, 252,1)
-combinations = list(product(SMA_S_range, SMA_L_range))
+# SMA_S_range = range(10,50,1)
+# SMA_L_range = range(100, 252,1)
+# combinations = list(product(SMA_S_range, SMA_L_range))
 
+test= data
+bnh = cantrarian(test)
+mom = momentum(test)
+sma = SMA_test(test, (50,200))
+print(bnh, mom, sma)
 
 
 
--- testing2.py
+++ testing2.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
-import numpy as np 
-from itertools import product 
 import BacktesterSMA as BSMA
-from scipy.optimize import brute
 plt.style.use("seaborn")
 
 
@@ -13,14 +10,12 @@ tickers = ["XI", "AMZN", "MSFT", "XRP-USD", "BA", "KO", "IBM", "DIS", "MSFT" ]
 def SMA(ticker):
     
     global df
-    stocks = yf.download(ticker, start = "2008-01-01", end = "2021-03-26")
+    stocks = yf.download([ticker, 'XRP-USD'], start = "2008-01-01", end = "2021-03-26")
     
     df = stocks.loc[:, "Close"].copy()
-    df = df.to_frame()
+
     print(df.columns)
-    tester = BSMA.SMABacktester(df, ticker[0], 50, 200, "2004-01-01", "2021-06-30")
-    
-    
+    tester = BSMA.SMABacktester(df, ticker, 50, 200, "2004-01-01", "2021-06-30")
     
     tester.get_data()
     tester.set_parameters(50,200)
@@ -31,7 +26,36 @@ def SMA(ticker):
 
 
 
-SMA(["XI"])
+SMA("KO")
+
+tickers = ["XI", "AMZN", "MSFT", "XRP-USD", "BA", "KO", "IBM", "DIS", "MSFT" ]
+stocks = yf.download(tickers, start = "2008-01-01", end = "2021-03-26")
+    
+df = stocks.loc[:, "Close"].copy()
+
+print(df.columns)
+tester = BSMA.SMABacktester(df, 'AMZN', 50, 200, "2004-01-01", "2021-06-30")
+    
+    
+tester.get_data()
+tester.set_parameters(50,200)
+    
+result = tester.test_strategy()
+print(result)
+tester.plot_results()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
